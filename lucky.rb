8c3b748d071246750110d2a7a3bd8d8d76f6cb9c
Joseph Tingsanchali <charnwoot@gmail.com>
2014-06-06 12:30:13 -0500

    initial commit with solution

--- lucky.rb
+++ lucky.rb
@@ -18,3 +18,52 @@
 # Credit to SundaySalsa on codewars.com for coming
 # up with this problem.
 
+class Lucky
+	def self.check(numb)
+	  raise :error if numb.size == 0
+		rts = numb.scan(/[a-zA-Z]|\s/)
+		raise :error if rts.size > 0
+		a = numb.to_s.split("")
+		index = a.length
+		total1 = a[0,index/2].inject(0) { |sum,n| sum + n.to_i}
+		if index%2 == 0
+			total2 = a[index/2,index].inject(0) { |sum,n| sum + n.to_i}
+		else
+			total2 = a[(index/2)+1,index].inject(0) { |sum,n| sum + n.to_i}
+		end
+		if total1 == total2
+			true
+		else
+			false
+		end
+	end
+end
+
+# Codegolf solution:
+# def luck_check (str)
+#   raise if str[/\D/] || str.empty?
+#   str = str.chars.map(&:to_i)
+#   str.first(str.size/2).inject(:+) == str.last(str.size/2).inject(:+)
+# end
+
+# # Nick's solution
+# class Lucky
+# 	def self.check str
+# 		raise :error if str.size == 0
+# 		rts = str.scan(/[a-zA-Z]|\s/)
+# 		raise :error if rts.size > 0
+
+# 		digits = str.scan(/\d/)
+# 		x = digits.size / 2
+
+# 		# y, z = digits[0..x-1], digits[-x..-1]
+# 		y = digits[0..x-1]
+# 		z = digits[-x..-1]
+
+# 		[y, z].each do |a|
+# 			a.map! { |b| b.to_i }
+# 		end
+
+# 		y.inject(:+) == z.inject(:+)
+# 	end
+# end
